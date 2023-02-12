ef4c5b15b9880637000806f153208322cf9b9476
ck1049 <18238489029@163.com>
2023-02-12 23:10:22 +0800

    引入nacos

---
+++ src/test/java/com/tulingxueyuan/springcloud/springcloudalibaba/SpringcloudalibabaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tulingxueyuan.springcloud.springcloudalibaba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudalibabaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
